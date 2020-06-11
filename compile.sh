707e42a1984186359df457e148e735e68b9ee7c5
Manfred Doudar <manfred@xplicitcomputing.com>
2020-06-11 14:11:29 +0800

    update compile.sh script so it is a little more extensible

--- compile.sh
+++ compile.sh
@@ -1,54 +1,82 @@
 #!/bin/bash
 
 
-languages='cpp csharp java javascript objc python ruby php' # go dart julia perl c r rust scala swift
+BDIR=bindings
+LANGUAGES='cpp csharp java javascript objc python ruby php' # go dart julia perl c r rust scala swift
 
-echo "Compiling protocol buffer messages for: $languages"
-[ "$iact" = off ] || read -p "Press [enter] to continue."
 
-#prep cmake environment in current directory
-do_clean() {
-    make clean
-    rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake Makefile
-    rm -rf bindings/${languages}
-}
-
-# if .buildopts exists, use it
-[ -f .buildopts ] && . .buildopts
-
+quiet=
 cmpargs=
+external_init=
 while [ $# -gt 0 ]; do
     case "$1" in
-      CC=*)
-        cmpargs="${cmpargs} $1"
-        ;;
-      clean)
-        do_clean
-        exit 0
-        ;;
-      *)        # currently, nothing else passes through
-        ;;
+        CC=*)
+            cmpargs="${cmpargs} $1"
+            ;;
+
+        clean)
+            if [[ -f Makefile ]]; then
+                make clean
+            fi
+            rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake Makefile
+            rm -rf ${BDIR}/${LANGUAGES}
+            exit 0
+            ;;
+
+        -e|--external)
+            external_init=true
+            ;;
+
+        -q|--quiet)
+            quiet=true
+            ;;
+
+        *)        # currently, nothing else passes through
+            ;;
     esac
     shift
 done
-cmake .
-# keep CPU in reserve for non-build
-ncpu=$(nproc)
-nrs=1           # CPUs reserved for non-build
-ncpu=$(( (ncpu>nrs)? ncpu-nrs : 1 ))
-#build the protocol buffers that are used by xcompute directly
-#utils/cmakeprep.sh ${cmpargs} .
-make -j${ncpu}
 
-#build the protobuf bindings that will be used by users and developers to integrate
-mkdir -p ${languages} #temporary directory for each language
-protoc --cpp_out=cpp --python_out=python --java_out=java --js_out=javascript,import_style=commonjs,binary:. --ruby_out=ruby --objc_out=objc --csharp_out=csharp --php_out=php vector.proto system.proto spatial.proto meta.proto
-mkdir -p bindings
-for lang in $languages
-do
-	cp -rf $lang bindings/ #move under bindings
-	rm -rf $lang
-done
+
+
+if [[ -z "${quiet}" ]]; then
+    echo "Compiling protocol buffer messages for: ${LANGUAGES}"
+fi
+[ "$iact" = off ] || read -p "Press [enter] to continue."
+
+
+
+# prep cmake environment in current directory
+if [[ -z "${external_init}" ]]; then
+
+    # if .buildopts exists, use it
+    [ -f .buildopts ] && . .buildopts
+
+    cmake .
+
+    # keep CPU in reserve for non-build
+    ncpu=$(nproc)
+    nrs=1           # CPUs reserved for non-build
+    ncpu=$(( (ncpu>nrs)? ncpu-nrs : 1 ))
+
+    # build the protocol buffers that are used by xcompute directly
+    #utils/cmakeprep.sh ${cmpargs} .
+    make -j${ncpu}
+fi
+
+
+# build the protobuf bindings that will be used by users and developers to integrate
+directories=`echo ${LANGUAGES} | sed -e "s/[^ ]* */${BDIR}\/&/g"`
+mkdir -p $directories
+protoc  --cpp_out=${BDIR}/cpp \
+        --python_out=${BDIR}/python \
+        --java_out=${BDIR}/java \
+        --js_out=${BDIR}/javascript,import_style=commonjs,binary:. \
+        --ruby_out=${BDIR}/ruby \
+        --objc_out=${BDIR}/objc \
+        --csharp_out=${BDIR}/csharp \
+        --php_out=${BDIR}/php \
+        vector.proto system.proto spatial.proto meta.proto
 
 
 [ "$iact" = off ] || read -p "Press [enter] to continue."
