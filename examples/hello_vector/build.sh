adbb50be4cf0a31282f049c5403e59b0bcb4b83c
Manfred Doudar <manfred@xplicitcomputing.com>
2020-07-12 19:14:00 +0800

    retab

--- examples/hello_vector/build.sh
+++ examples/hello_vector/build.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-# make links to required library and header (for convenience)
-ln -f ../../libxcmessages.a libxcmessages.a
-ln -f ../../vector.pb.h vector.pb.h
 
-# invoke c++ compiler on save source to create app:
-c++ save_msg.cpp -o save_app libxcmessages.a -lprotobuf
-# invoke c++ compiler on load source to create app:
-c++ load_msg.cpp -o load_app libxcmessages.a -lprotobuf
+if [[ ! -z "${GOOGLE_ROOT}" ]]; then
 
-read -p "Press [enter] to close."
+    ln -fs ../../vector.pb.h
+
+    c++ save_msg.cpp -o save_app -L../.. -lxcmessages -L${GOOGLE_ROOT}/lib -lprotobuf
+    c++ load_msg.cpp -o load_app -L../.. -lxcmessages -L${GOOGLE_ROOT}/lib -lprotobuf
+
+else
+
+    # make links to required library and (for convenience)
+    ln -f ../../libxcmessages.a libxcmessages.a
+    ln -f ../../vector.pb.h vector.pb.h
+
+    # invoke c++ compiler on save source to create app:
+    c++ save_msg.cpp -o save_app libxcmessages.a -lprotobuf
+
+    # invoke c++ compiler on load source to create app:
+    c++ load_msg.cpp -o load_app libxcmessages.a -lprotobuf
+
+    read -p "Press [enter] to close."
+fi
--- examples/hello_vector/load_msg.cpp
+++ examples/hello_vector/load_msg.cpp
@@ -1,52 +1,72 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <cmath> 
-
 #include "vector.pb.h"
 
-using namespace std;
+#include <cmath> 
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
-string name{"sine-sweep.xco"}; // file name
 
 
-//SAVE protobuf msg
-bool load(Messages::Vector64& msg){
-	//2a. open a file stream
-	ifstream infile(name);
-	if (!infile.is_open()){//handle errors
-		cerr << "error opening file: " << name << endl;
-		cout << strerror(errno) << endl;
-		return false;
-	}
-	cout << "opened file: " << name << endl;
-	//2b. bytes are streamed from protobuf into the output file
-	msg.ParseFromIstream(&infile);
-	//2c. close the file when done
-	infile.close();
-	return true;
+
+std::string name{ "sine-sweep.xco" };                                               // file name
+
+
+
+bool load(Messages::Vector64& msg)
+{
+    // SAVE protobuf msg
+
+
+    // 2a. open a file stream
+    std::ifstream infile(name);
+    if (!infile.is_open())
+    {
+        // handle errors
+
+        std::cerr << "error opening file: " << name << std::endl;
+        std::cout << strerror(errno) << std::endl;
+
+        return false;
+    }
+    std::cout << "opened file: " << name << std::endl;
+
+    // 2b. bytes are streamed from protobuf into the output file
+    msg.ParseFromIstream(&infile);
+
+    // 2c. close the file when done
+    infile.close();
+
+    return true;
 }
 
 
-//MAIN FUNCTION
-int main(int argc, char** argv){
-	if (argc > 1)//can change name by passing in
-		name = argv[1];
-    cout << "launching load app for: " << name << "..." << endl;
-	//1. create a new protobuf message (see vector.proto for what's available):
-	Messages::Vector64 msg;
 
-	//2. when ready to save or transmit
-	load(msg);
+int main(int argc, char** argv)
+{
+    if (argc > 1)//can change name by passing in
+    {
+        name = argv[1];
+    }
+    std::cout << "launching load app for: " << name << "..." << std::endl;
 
-	//3. print some stats to show we've actually reconstructed it
-	cout << "data name: " << msg.name() << endl;
-	cout << "components: " << msg.components() << endl;
-	cout << "length: " << msg.values_size()/std::max(1,msg.components()) << endl;
-	cout << "size: " << msg.values_size() << endl;
-	cout << "bytes: " << msg.values_size()*sizeof(double) << endl;
-	//4. do something else with it, like load into a vector and compute...
-	cout << "closing load app." << endl;
-	//message should be stored on file in this directory
+    // 1. create a new protobuf message (see vector.proto for what's available):
+    Messages::Vector64 msg;
+
+    // 2. when ready to save or transmit
+    load(msg);
+
+    // 3. print some stats to show we've actually reconstructed it
+    std::cout << "data name: " << msg.name() << std::endl;
+    std::cout << "components: " << msg.components() << std::endl;
+    std::cout << "length: " << msg.values_size() / std::max(1,msg.components()) << std::endl;
+    std::cout << "size: " << msg.values_size() << std::endl;
+    std::cout << "bytes: " << msg.values_size() * sizeof(double) << std::endl;
+
+    // 4. do something else with it, like load into a vector and compute...
+    std::cout << "closing load app." << std::endl;
+
+    // message should be stored on file in this directory
+
     return 0;
 }
