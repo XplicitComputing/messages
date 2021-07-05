fb59eeaa6e7814c9d06188c068720fe7def0567f
graham <graham@xplicitcomputing.com>
2021-07-05 00:25:45 -0700

    fixed protobuf dep script, added size_hint field to Messages::Vector to aid in defining structured filters, etc.

--- deps/install-all-brew.sh
+++ deps/install-all-brew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo ./install-deps-brew.sh
-sudo ./install-protobuf.sh
+sudo ./install-deps-brew.sh --dpfx=/usr/local
+sudo ./install-protobuf.sh --prefix=/usr/local
 sudo ./install-plugin-c.sh
 sudo ./install-plugin-go.sh
 sudo ./install-plugin-dart.sh
