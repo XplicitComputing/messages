d6db1f9873150bbcbda1547adc96127ae20a9ecb
R. J. Dev. Kwan <6440263+kwan0xfff@users.noreply.github.com>
2021-11-24 00:40:01 -0800

    Overhaul install-deps scripts for messages.
    * Encapsulate argument parsing as getargs().
    * Use clang-12 instead of -10.

--- deps/install-deps-apt.sh
+++ deps/install-deps-apt.sh
@@ -3,37 +3,48 @@ echo "* * * * * * * *  XPLICIT COMPUTING BUILD ENVIRONMENT (MESSAGES) * * * * * 
 
 dpfx=
 
-while [ $# -gt 0 ]; do
-    case "$1" in
-      --dpfx=*)                 # dependencies prefix
-        dpfx="${1#*=}"
-        ;;
-      *)  # unknown argument; don't care.
-        ;;
-    esac
-    shift
-done
+getargs() {
+    while [ $# -gt 0 ]; do
+        case "$1" in
+          --dpfx=*)                 # dependencies prefix
+            dpfx="${1#*=}"
+            ;;
+          *)  # unknown argument; don't care.
+            ;;
+        esac
+        shift
+    done
 
-if [ -z "${dpfx}" ]; then
-    echo missing "dependencies prefix: --dpfx=..."
-    echo For final product build, try: --dpfx=/usr/local/xcompute
-    echo For development builds, try : '--dpfx=${HOME}/xcompute' or deps dir of your choosing
-    exit 1
-fi
+    if [ -z "${dpfx}" ]; then
+        echo missing "dependencies prefix: --dpfx=..."
+        echo For final product build, try: --dpfx=/usr/local/xcompute
+        echo For development builds, try : '--dpfx=${HOME}/xcompute'
+        echo ... or deps dir of your choosing
+        exit 1
+    fi
+}
+
+getargs $*
+
 echo "Installing compiler and build tools..."
-sudo apt install clang clang-10 libstdc++-10-dev
+sudo apt install clang-12 libstdc++-10-dev
 sudo apt install cmake automake autoconf autogen libtool checkinstall wget curl unzip
+
 echo "Your current C compiler is:"
 which cc
 cc --version
+
 echo "Your current C++ compiler is:"
 which c++
 c++ --version
-echo "Attempting to set your default C/C++ compilers to clang-10 :"
-sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang-10 60
-sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-10 60
+
+echo "Attempting to set your default C/C++ compilers to clang-12 :"
+sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang-12 60
+sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-12 60
+
 echo "Your current C++ compiler is:"
 which c++
 c++ --version
+
 read -p "Press [enter] to proceed with Protobuf3 installation..."
 sudo ./install-protobuf.sh --prefix=${dpfx}
