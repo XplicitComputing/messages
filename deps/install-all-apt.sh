2d8375f5b862b0b9d0bf01aa96992cf8947de714
R. J. Dev. Kwan <6440263+kwan0xfff@users.noreply.github.com>
2022-01-01 14:35:59 -0800

    Add '--dpfx=' argument parsing, remove sudo from script invocations.
    * 'install-all-apt.sh' had hard-coded "--dpfx=/usr/local", which
      will work for some developers and not others. This allows them to
      set the dependencies prefix as needed for their environments.
    * Build scripts (with configure, make, etc.) should not be invoked with
      'sudo'.  In this case, the scripts being invoked already have 'sudo'
      in them, and thus make parent script 'sudo' unnecessary.

--- deps/install-all-apt.sh
+++ deps/install-all-apt.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
-sudo ./install-deps-apt.sh --dpfx=/usr/local
-sudo ./install-plugin-c.sh
-sudo ./install-plugin-go.sh
-sudo ./install-plugin-dart.sh
+
+dpfx=
+
+getargs() {
+    while [ $# -gt 0 ]; do
+        case "$1" in
+          --dpfx=*)                 # dependencies prefix
+            dpfx="${1#*=}"
+            ;;
+          *)  # unknown argument; don't care.
+            ;;
+        esac
+        shift
+    done
+
+    if [ -z "${dpfx}" ]; then
+        echo missing "dependencies prefix: --dpfx=..."
+        echo For final product build, try: --dpfx=/usr/local/xcompute
+        echo For development builds, try : '--dpfx=${HOME}/xcompute'
+        echo ... or deps dir of your choosing
+        exit 1
+    fi
+}
+
+getargs $*
+
+./install-deps-apt.sh --dpfx=${dpfx}
+./install-plugin-c.sh
+./install-plugin-go.sh
+./install-plugin-dart.sh
