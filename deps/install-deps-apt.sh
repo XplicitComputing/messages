ae57924812797b07dfa0bb665b16cf6c7488c67b
graham <graham@xplicitcomputing.com>
2022-02-24 11:53:30 -0800

    semi-important commit. minor spec change to Messages::Zone (Region) and cleanup on path to release (such as minor name changes that will cause trivial compilation breaks downstream. added Messages::Record64 to support encapsulated dataset management (such as with materials properties).

--- deps/install-deps-apt.sh
+++ deps/install-deps-apt.sh
@@ -3,51 +3,38 @@ echo "* * * * * * * *  XPLICIT COMPUTING BUILD ENVIRONMENT (MESSAGES) * * * * * 
 
 dpfx=
 
-getargs() {
-    while [ $# -gt 0 ]; do
-        case "$1" in
-          --dpfx=*)                 # dependencies prefix
-            dpfx="${1#*=}"
-            ;;
-          *)  # unknown argument; don't care.
-            ;;
-        esac
-        shift
-    done
+while [ $# -gt 0 ]; do
+    case "$1" in
+      --dpfx=*)                 # dependencies prefix
+        dpfx="${1#*=}"
+        ;;
+      *)  # unknown argument; don't care.
+        ;;
+    esac
+    shift
+done
 
-    if [ -z "${dpfx}" ]; then
-        echo missing "dependencies prefix: --dpfx=..."
-        echo For final product build, try: --dpfx=/usr/local/xcompute
-        echo For development builds, try : '--dpfx=${HOME}/xcompute'
-        echo ... or deps dir of your choosing
-        exit 1
-    fi
-}
-
-getargs $*
-
-echo "Installing compiler and build tools..."
-sudo apt install clang-12 libstdc++-10-dev
-sudo apt install cmake automake autoconf autogen libtool checkinstall wget curl unzip
-
-echo "Install application dev dependencies..."
-sudo apt install libncurses-dev # not used directly by "messages".
-
+if [ -z "${dpfx}" ]; then
+    echo missing "dependencies prefix: --dpfx=..."
+    echo For final product build, try: --dpfx=/usr/local/xcompute
+    echo For development builds, try : '--dpfx=${HOME}/xcompute' or deps dir of your choosing
+    exit 1
+fi
+echo "Installing compiler and autotools..."
+sudo apt install clang clang-12
+sudo apt install cmake automake autoconf autogen libtool checkinstall wget curl unzip zlib1g-dev
 echo "Your current C compiler is:"
 which cc
 cc --version
-
 echo "Your current C++ compiler is:"
 which c++
 c++ --version
-
-echo "Attempting to set your default C/C++ compilers to clang-12 :"
+echo "Please set the C compiler to clang-12 before proceeding with XC builds:"
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang-12 60
+echo "Please set the C++ compiler to clang++-12:"
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-12 60
-
-echo "Your current C++ compiler is:"
+echo "The current C++ compiler is:"
 which c++
 c++ --version
-
 read -p "Press [enter] to proceed with Protobuf3 installation..."
-./install-protobuf.sh --prefix=${dpfx}
+sudo ./install-protobuf.sh --prefix=${dpfx}
--- deps/install-deps-brew.sh
+++ deps/install-deps-brew.sh
@@ -3,41 +3,40 @@ echo "* * * * * * * *  XPLICIT COMPUTING BUILD ENVIRONMENT (MESSAGES) * * * * * 
 
 dpfx=
 
-getargs() {
-    while [ $# -gt 0 ]; do
-        case "$1" in
-          --dpfx=*)                 # dependencies prefix
-            dpfx="${1#*=}"
-            ;;
-          *)  # unknown argument; don't care.
-            ;;
-        esac
-        shift
-    done
+while [ $# -gt 0 ]; do
+    case "$1" in
+      --dpfx=*)                 # dependencies prefix
+        dpfx="${1#*=}"
+        ;;
+      *)  # unknown argument; don't care.
+        ;;
+    esac
+    shift
+done
 
-    if [ -z "${dpfx}" ]; then
-        echo missing "dependencies prefix: --dpfx=..."
-        echo For final product build, try: --dpfx=/usr/local/xcompute
-        echo For development builds, try : '--dpfx=${HOME}/xcompute'
-        echo ... or deps dir of your choosing
-        exit 1
-    fi
-}
-
-getargs $*
-
-echo "Installing autotools..."
-#brew install clang-12 libstdc++-10-dev
+if [ -z "${dpfx}" ]; then
+    echo missing "dependencies prefix: --dpfx=..."
+    echo For final product build, try: --dpfx=/usr/local/xcompute
+    echo For development builds, try : '--dpfx=${HOME}/xcompute' or deps dir of your choosing
+    exit 1
+fi
+echo "Installing compiler and autotools..."
 brew install cmake automake autoconf autogen libtool checkinstall wget curl unzip
-echo "Your c++ compiler should be at least clang++ 12, provided by Apple Xcode."
-
 echo "Your current C compiler is:"
 which cc
 cc --version
-
 echo "Your current C++ compiler is:"
 which c++
 c++ --version
-
+#echo "Consider setting your default compiler to clang-12 before proceeding with XC builds:"
+#echo "Please set your default C compiler to clang-12 :"
+#sudo update-alternatives --config cc
+#sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang-12 60
+#echo "Please set your default C++ compiler to clang++-12 :"
+#sudo update-alternatives --config c++
+#sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-12 60
+#echo "Your current C++ compiler is:"
+#which c++
+#c++ --version
 read -p "Press [enter] to proceed with Protobuf3 installation..."
 sudo ./install-protobuf.sh --prefix=${dpfx}
