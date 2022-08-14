b48e2653b58cbb742380e5ae8fa6ba114e17530d
R. J. Dev. Kwan <6440263+kwan0xfff@users.noreply.github.com>
2022-08-14 10:49:40 -0700

    Major rewrite of install-protobuf-brew.sh.
    * Use 4-pass for-loop to fetch tar.gz and check MD5 sum.
    * Default protobuf*tar.gz MD5 sum for 3.8.0.
    * Improve detection/warning of differing protobuf (protoc) version.
    * --notag is deprecated.
    * remove ldconfig; macOS doesn't use it.

--- deps/install-protobuf-brew.sh
+++ deps/install-protobuf-brew.sh
@@ -3,40 +3,46 @@
 # NOTE: THIS IS A DEVELOPMENTAL VERSION FOR MACOS.
 #
 # Build and install Protocol Buffer library from Github source.
-# Remove Ubuntu/Debian package first, if it exists.
 # options:
 #   --tag=      Checkout code at a tag on current branch (default to $TAG).
-#   --notag     Checkout HEAD (latest); no tag.
 #   --prefix=   Install directory prefix (default to $PREFIX)
 #   --noclean   Do not remove build directory after build and install.
+#   --getmd5    Read archived file by tag, determine MD5 sum.
+#   --md5=      Specify MD5 sum for tagged file.
 #
 # Examples:
-#   ./install-protobuf.sh --notag --noclean         # Use latest source, do not remove build.
 #   ./install-protobuf.sh --prefix=/opt/xcompute    # Set install target to /opt/xcompute
 #   ./install-protobuf.sh CXX=clang++               # Pass CXX through to package build.
 #
+
+# requires Bash 3.0 (for i in {x..y})
 
 RED='\033[1;31m'        # bold red
 YLW='\033[1;33m'        # bold yellow
 NC='\033[0m'            # No Color
 YLW_WARNING="${RED}[${YLW}WARNING${RED}]${NC}"
 
-PKG_PROTOBUFDEV=libprotobuf-dev                 # dev package on Ubuntu/Debian
-TAG=3.8.0                                   # Known recent release tag
-PREFIX=/usr/local   # Install target prefix
+GH_PROTOBUF_RLSEDL=https://github.com/protocolbuffers/protobuf/releases/download
+PREFIX=/usr/local       # Install target prefix
 CLEANUP=1
 CFGARGS="CFLAGS=-fPIC CXXFLAGS=-fPIC"
+TAG=3.8.0               # Default protobuf release for early XC Messages
+PBMD5=cc4f50740430d9488312e5e48bc94d68  # v3.8.0
+GETMD5=
 
 while [ $# -gt 0 ]; do
     case "$1" in
       --tag=*)
         TAG="${1#*=}"
         ;;
-      --prefix=*)
+      --dpfx=*|--prefix=*)
         PREFIX="${1#*=}"
         ;;
-      --notag)
-        TAG=
+      --md5=*)
+         PBMD5="${1#*=}"
+        ;;
+      --getmd5)
+      GETMD5=y
         ;;
       --noclean)
         CLEANUP=
@@ -47,6 +53,15 @@ while [ $# -gt 0 ]; do
     esac
     shift
 done
+
+pbtar=protobuf-all-${TAG}.tar.gz
+
+if [ "${GETMD5}" = y ]; then
+    echo Compute MD5 sum:
+    wget -O- -q ${GH_PROTOBUF_RLSEDL}/v${TAG}/${pbtar} | openssl md5
+    echo Complated.
+    exit 0
+fi
 
 if [ \! -d ${PREFIX} ]; then
     echo Target install directory \"${PREFIX}\" does not exist.
@@ -59,36 +74,72 @@ echo Install target $PREFIX
 
 echo "---- Installing Protocol Buffers ----"
 
-#dpkg -l ${PKG_PROTOBUFDEV}
 
-#rc_dpkg=$?
-#if [ $rc_dpkg -eq 0 ]; then
-#    echo -e ${YLW_WARNING}: a different version of ${YLW}${PKG_PROTOBUFDEV}${NC} is installed on your machine
-#    echo Currently using $(which protoc)
-#    echo Please make sure that the right version is found in PATH at build-time.
-#    sleep 2
-#fi
+getmd5() {
+    local fname=$1
+    retval=$(openssl md5 ${fname} |  awk '{print $2}')
+}
 
-# Check for existence of tar file and correctness.
-cat <<- _end_text > protobuf.md5
-cc4f50740430d9488312e5e48bc94d68  protobuf-all-3.8.0.tar.gz
-_end_text
-#md5sum -c protobuf.md5
-# Eventually, use compare the sums; for now, openssl|awk simply succeeds.
-openssl md5 protobuf.md5 | awk '{print $2}' 
-rc_md5=$?
-if [ ! -f protobuf-all-3.8.0.tar.gz ]; then
-    wget https://github.com/protocolbuffers/protobuf/releases/download/v${TAG}/protobuf-all-${TAG}.tar.gz
-    # perhaps check MD5 sum in a limited loop
+protocvers() {
+    retval=$(protoc --version | awk '{print $2}')
+    echo protoc vers ${retval}
+}
+
+for fetchpass in {1..4}; do
+
+    if [ -f ${pbtar} ]; then
+        getmd5 ${pbtar}; pbtar_md5=${retval}
+        if [ ${pbtar_md5} != ${PBMD5} ]; then
+            matched=fail
+        else
+            matched=succ
+        fi
+    else
+        matched=fail
+    fi
+    # matched=fail        # for testing
+    if [ ${matched} = succ ]; then
+        break
+    else
+        echo fetchpass ${fetchpass}...
+        if [ -f ${pbtar} ]; then rm ${pbtar}; fi
+        wget ${GH_PROTOBUF_RLSEDL}/v${TAG}/${pbtar}
+    fi
+
+done
+#echo last fetchpass ${fetchpass}...
+#echo matched ${matched}
+if [ ${matched} = fail ]; then
+    echo Unable to fetch protobuf source ${pbtar}
+    exit 1
 fi
-tar -xf protobuf-*.tar.gz
+
+# check for pre-existing incompatible protoc.
+which protoc
+rc_which=$?
+if [ $rc_which -eq 0 ]; then
+    protocvers; pbvers=retval
+    if [ ${pbvers} != ${TAG} ]; then
+        echo -e ${YLW_WARNING}: a different version of ${YLW}protoc${NC} is installed on your machine
+        echo Currently using $(which protoc)
+        echo -e ${YLW}Please make sure that the right version is found in PATH at build-time.${NC}
+        sleep 3
+        for count in 5 4 3 2 1; do
+            echo -n ${count}'... '
+            sleep 1
+        done
+        echo ' '
+    fi
+fi
+
+tar -xf ${pbtar}
 cd protobuf-${TAG}
 
 ./autogen.sh
 ./configure --prefix=${PREFIX} ${CFGARGS}
 ncpu=$(sysctl -n hw.ncpu)
-make -j$(ncpu)         #
-make check # -j$(npcu)
+make -j${ncpu}
+make check -j${npcu}
 
 # check if target needs 'sudo'; execute according
 xcuid=$(stat -c '%u' ${PREFIX})
@@ -99,20 +150,12 @@ fi
 
 set -x
 ${PRIV} make install
-# ldconfig for libprotobuf needs 'autoconf --disable-shared';
-# tells autoconf/libtool to not build shared libraries.
-[ -n "${PRIV}" ] && ${PRIV} ldconfig
-cd ..
-set +x
 
 if [ "$CLEANUP" ]; then
     echo "Cleaning..."
-    rm -R protobuf*/
+    cd ..
+    rm -R protobuf-${TAG}
 fi
 export PATH=${PREFIX}/bin:$PATH
 which protoc
 protoc --version
-#if [ $rc_dpkg -eq 0 ]; then
-#    echo -e ${YLW_WARNING}: remember to avoid system installed version of ${YLW}${PKG_PROTOBUFDEV}${NC}
-#    dpkg -l ${PKG_PROTOBUFDEV} | grep protobuf
-#fi
